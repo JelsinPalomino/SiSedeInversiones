10fa3d0e1ba99a0c11af6074bbf9ad0aef4d7ce0
JelsinPalomino <jstpalomino@hotmail.com>
2025-01-28 18:41:41 -0500

    [updated-format] usamos isort

--- tests/test_lista_ejecucion.py
+++ tests/test_lista_ejecucion.py
@@ -1,6 +1,5 @@
-from sisedeinversiones.lista_ejecucion.lista_ejecucion_simple import (
-    ScrapingListaEjecucion,
-)
+from sisedeinversiones.lista_ejecucion.lista_ejecucion_simple import \
+    ScrapingListaEjecucion
 
 file_read = "tests/test_CUI_2019_dep7.csv"
 path_export = "tests"
